e1e18bf2070aed52492a6626e29328ac6a114a02
Arka Sain <arka.sain@aol.com>
2021-06-06 14:03:25 +0200

    Testing `promises` with Mocha

--- app/order.js
+++ app/order.js
@@ -1,5 +1,7 @@
-module.exports = (cart, cb) => {
-    setTimeout(() => {
-        cb(true)
-    }, 500)
+module.exports = (cart) => {
+    let p = new Promise((resolve, reject) => {
+        resolve(500);
+    });
+
+    return p;
 }
--- app/order.spec.js
+++ app/order.spec.js
@@ -2,15 +2,11 @@ const order = require('./order');
 const expect = require('chai').expect;
 
 describe('order', () => {
-    it('should call the callback with a true value', (done) => {
+    it('should deal with promises', () => {
         let cart = {};
-        let success;
-        let cb = (p1) => {
-            success = p1;
-            expect(success).to.be.true;
-            done();
-        }
-
-        order(cart, cb);
+        
+        return order(cart).then(total => {
+            expect(total).to.eq(500);
+        });
     });
 });
