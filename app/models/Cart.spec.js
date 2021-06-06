7a610576f2eea84e9eb2deeaf8954795ac55ca01
Arka Sain <arka.sain@aol.com>
2021-06-06 12:16:32 +0200

    Testing business logic with `Mocha`

--- app/models/Cart.spec.js
+++ app/models/Cart.spec.js
@@ -22,5 +22,32 @@ describe('Cart', () => {
             expect(cart.lineItems).to.have.lengthOf(1);
             expect(cart.lineItems[0].quantity).to.eq(2);
         });
+
+        it('should add quantities together when addItem is called', () => {
+            cart.addItem(myPart, 2);
+            cart.addItem(myPart, 4);
+
+            expect(cart.lineItems).to.have.lengthOf(1);
+            expect(cart.lineItems[0].quantity).to.eq(6);
+        });
+
+        it('should have two items if addItem is called with different parts', () => {
+            let myPart2 = {};
+            cart.addItem(myPart, 1);
+            cart.addItem(myPart2, 1);
+
+            expect(cart.lineItems).to.have.lengthOf(2);
+        });
+
+        it('should add quantity to their correct existing item', () => {
+            let myPart2 = {};
+            cart.addItem(myPart, 1);
+            cart.addItem(myPart2, 1);
+
+            cart.addItem(myPart, 3);
+
+            expect(cart.lineItems).to.have.lengthOf(2);
+            expect(cart.lineItems[0].quantity).to.eq(4);
+        });
     });
 });
